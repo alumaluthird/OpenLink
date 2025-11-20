485793b2504a29940eff9b45fd27a17fcfdb49a0
alumaluthird <12880915+alumaluthird@users.noreply.github.com>
2025-11-20 17:25:00 +0900

    Add core package exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,8 @@
+export { WalletManager } from './wallet-manager';
+export { OpenLinkEventEmitter } from './event-emitter';
+export * from './types';
+export * from './utils';
+
+// Main export
+export { WalletManager as OpenLink } from './wallet-manager';
+
