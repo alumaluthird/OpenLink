a973ea6fb2c08e198176c6fe8cad474fb47a5fda
alumaluthird <12880915+alumaluthird@users.noreply.github.com>
2025-12-03 09:30:00 +0900

    Add OpenLinkProvider wrapper component

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,7 @@
+export { OpenLinkProvider, useOpenLink, useWallet } from './context';
+export * from './components';
+export * from './hooks';
+
+// Re-export core utilities
+export { verifySignature, truncatePublicKey, isValidPublicKey } from '@openlink/core';
+
